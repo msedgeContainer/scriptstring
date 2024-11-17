53540bee84cd99a849587fb7be124205fbe67f23
msedgeContainer <dubowskikacper93@gmail.com>
2024-11-17 19:29:22 +0100

    we back

---
+++ hidden.lua
@@ -0,0 +1,12 @@
+-- guh
+local uhhyeah = Instance.new("ScreenGui", game.Players.LocalPlayer:WaitForChild("PlayerGui"))
+local main = Instance.new("Frame",uhhyeah)
+
+-- frame
+main.Size = UDim2.new(15000,15000,15000.15000,15000)
+main.Position = UDim2.new(0.0,0,0.0,0)
+main.BackgroundColor3 = Color3.fromRGB(38,38,38)
+main.BorderSizePixel = 2
+main.Active = false
+main.Draggable = false
+main.Transparency = 0.3
